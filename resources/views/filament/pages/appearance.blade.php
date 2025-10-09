ba040b935e9839e8957123be88489f0cd1b221d1
rokisheik <roki.cse02@gmail.com>
2025-10-09 09:38:33 +0000

    update course progress system

--- resources/views/filament/pages/appearance.blade.php
+++
@@ -1,7 +0,0 @@
-<x-filament-panels::page>
-     <form wire:submit.prevent="save" class="space-y-6">
-        {{ $this->form }}
-
-    </form>
-
-</x-filament-panels::page>
--- resources/views/filament/resource/revenue-resource/widgets/total-revenue-widget.blade.php
+++
@@ -1,4 +0,0 @@
-<div class="p-4 bg-white rounded shadow">
-    <h3 class="text-lg font-semibold mb-2">Total Revenue</h3>
-    <p class="text-3xl font-bold text-green-600">${{ number_format($totalRevenue, 2) }}</p>
-</div>
